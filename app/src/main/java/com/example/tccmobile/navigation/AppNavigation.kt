9e23038e3cc2a8ce19f3bde561eab03b2b8c62fa
Breno <brenosg2310@gmail.com>
2025-11-21 02:07:29 -0300

    Adicionado navegação direto para a tela de home (para debug)

--- app/src/main/java/com/example/tccmobile/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tccmobile/navigation/AppNavigation.kt
@@ -6,12 +6,47 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tccmobile.ui.screens.loginScreen.LoginScreen
 import com.example.tccmobile.ui.screens.registerScreen.RegisterScreen
+import com.example.tccmobile.ui.screens.studentTicketsScreen.DashboardTicketsScreen
 
+//@Composable
+//fun AppNavigation() {
+//    val navController = rememberNavController()
+//
+//    NavHost(navController = navController, startDestination = Routes.LOGIN) {
+//        composable(Routes.LOGIN) {
+//            LoginScreen(
+//                onNavigateToRegister = {
+//                    navController.navigate(Routes.REGISTER)
+//                },
+//                onLoginSuccess = {
+//                    navController.navigate(Routes.HOME) {
+//                        popUpTo(Routes.LOGIN) { inclusive = true }
+//                    }
+//                }
+//            )
+//        }
+//        composable(Routes.REGISTER) {
+//            RegisterScreen(
+//                onNavigateToLogin = {
+//                    navController.popBackStack()
+//                },
+//                onRegisterSuccess = {
+//                    navController.popBackStack()
+//                }
+//            )
+//        }
+//        // composable(Routes.HOME) { /* ...Sua tela principal... */ }
+//    }
+//}
+
+
+// IR DIRETO PARA A TELA DOS TICKETS (SÓ DESCOMENTAR O QUE ESTÁ EMBAIXO E COMENTAR ACIMA) PARA TESTAR
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Routes.LOGIN) {
+    NavHost(navController = navController, startDestination = Routes.HOME) {
+
         composable(Routes.LOGIN) {
             LoginScreen(
                 onNavigateToRegister = {
@@ -24,6 +59,7 @@ fun AppNavigation() {
                 }
             )
         }
+
         composable(Routes.REGISTER) {
             RegisterScreen(
                 onNavigateToLogin = {
@@ -34,6 +70,18 @@ fun AppNavigation() {
                 }
             )
         }
-        // composable(Routes.HOME) { /* ...Sua tela principal... */ }
+
+        composable(Routes.HOME) {
+            DashboardTicketsScreen (
+                onNavigate = { route ->
+                    if (route != Routes.HOME) {
+                        navController.navigate(route)
+                    }
+                },
+                onTicketClick = { ticketId ->
+                    println("Cliquei no ticket: $ticketId")
+                }
+            )
+        }
     }
 }
