5b5aec433f857ad4ba7eef2808858d4dea9d2f18
LaraCruz18 <laravsc@hotmail.com>
2025-11-23 17:33:00 -0300

    feat: novo componente StatusBadge

---
+++ app/src/main/java/com/example/tccmobile/ui/components/utils/StatusBadge.kt
@@ -0,0 +1,51 @@
+package com.example.tccmobile.ui.components.utils
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tccmobile.ui.theme.Orange
+import com.example.tccmobile.ui.theme.SuperLightOrange
+
+
+//basicamente esse componente vai mostrar o status do ticket
+@Composable
+fun StatusBadge(
+    text: String,
+    backgroundColor: Color = SuperLightOrange,
+    textColor: Color = Orange
+) {
+    Box(
+        modifier = Modifier
+            .background(backgroundColor, shape = RoundedCornerShape(50))
+            .padding(horizontal = 12.dp, vertical = 4.dp)
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            style = androidx.compose.ui.text.TextStyle(
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp
+            )
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun StatusBadgePreview() {
+    StatusBadge(
+        text = "pendente",
+        backgroundColor = SuperLightOrange,
+        textColor = Orange
+    )
+}
