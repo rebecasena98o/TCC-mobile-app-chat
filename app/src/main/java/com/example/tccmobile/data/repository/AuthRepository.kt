14d7edca4a6b14af4839c361d9f6334d96808a77
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-11-25 15:50:03 -0300

    feat: add sign-in supabase method

--- app/src/main/java/com/example/tccmobile/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/tccmobile/data/repository/AuthRepository.kt
@@ -8,8 +8,35 @@ import com.example.tccmobile.data.supabase.SupabaseClient.client
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
 import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.postgrest.query.Columns
 
 class AuthRepository {
+
+    suspend fun signInStudent(registry: String, password: String): Boolean{
+        return try {
+
+            Log.d("SUPABASE_DEBUG", "registry: $registry")
+
+            val user = client.postgrest.from("users").select {
+                filter {
+                    eq("registry", registry)
+                }
+            }.decodeSingle<UserDto>()
+
+            Log.d("SUPABASE_DEBUG", "user: ${user.toString()}")
+
+            val auth = client.auth.signInWith(Email){
+                this.email = user.email
+                this.password = password
+            }
+
+            Log.d("SUPABASE_DEBUG", "Auth retornou: $auth")
+            true
+        }catch (e: Exception){
+            Log.e("SUPABASE_DEBUG", "ERRO NO SIGNIN", e)
+            false
+        }
+    }
 
     suspend fun signUpStudent(student: Student,  password: String): Boolean{
         return try {
@@ -25,6 +52,7 @@ class AuthRepository {
                 name = student.name,
                 email = student.email,
                 registry = student.registry,
+                createAt = null
             )
 
            client.postgrest.from("users")
