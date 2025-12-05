6dd3ac4a779dab44e6d342849321d2f0b66e173a
Kauam <kauam.morais09@gmail.com>
2025-12-04 22:12:29 -0300

    feat: Add variables studentProfile and librarianProfile

--- app/src/main/java/com/example/tccmobile/navigation/Routes.kt
+++ app/src/main/java/com/example/tccmobile/navigation/Routes.kt
@@ -5,4 +5,8 @@ object Routes {
     const val REGISTER = "register"
     const val HOME = "home"
     const val NEW_TICKET = "new-ticket"
+
+    const val STUDENT_PROFILE_SCREEN = "student-profile"
+
+    const val LIBRARIAN_PROFILE_SCREEN = "librarian-profile"
 }
