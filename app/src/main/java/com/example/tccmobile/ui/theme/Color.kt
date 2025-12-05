23b0aef311754dfd2ec66732d9f6819e6e049a77
Rebeca Sena <r3b3casena@gmail.com>
2025-12-05 18:10:43 -0300

    Corrigi algumas cores duplicadas e nomes das variáveis

--- app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
@@ -1,23 +1,10 @@
 package com.example.tccmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val DarkBlueBackground = Color(0xFF062452) // Fundo do BoxForm
-val PrimaryBlue = Color(0xFF0282EA)       // Botão "Entrar"
-val DarkBlueButton = Color(0xFF062452)    // Botão de toggle ativo
-val TextPrimary = Color.White
-
 val ClaroBlueBackground = Color(0xFFB8D1FF)
-val TextSecondary = Color(0xFFB0B0B0) // Placeholder/Helper
-
 val HeaderBackground = Color(0xFF204879)
-val BackgroundStart = Color(0xFF2287FF)    // Início do Gradiente
-val BackgroundEnd = Color(0xFF034384)      // Fim do Gradiente
-
 val Purple80 = Color(0xFFD0BCFF)
-
 val containerBlue = Color(0xFF003366)
-
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFF062452)
 
@@ -28,9 +15,7 @@ val CianoAzul = Color(0xFF0282EA)
 val Branco = Color(0xFFFFFFFF)
 
 val AzulClaro = Color(0xFF2287FF)
-
 val AzulLetra = Color(0xFF003366)
-
 val AzulSuperClaro = Color(0xFFC0E0FF)
 
 val Cinza = Color(0xFF64748B)
@@ -38,8 +23,6 @@ val Cinza = Color(0xFF64748B)
 val VermelhoTelha = Color(0xFF973C00)
 
 val AmareloClaro1 = Color(0xFFFEE685)
-
-val AmareloClaro2 = Color(0xFFFEF3C6)
 
 val Laranja = Color(0xFFFFD6A7)
 
@@ -71,37 +54,22 @@ val Pink40 = Color(0xFFFF004F)
 
 val white = Color(0xFFFFFFFF)
 
-val black = Color(0xFF000000)
+
 
 val yellow = Color(0xFFF6E7B4)
-
 val green = Color(0xFF00A63E)
-
-val VermelhoTelha = Color(0xFF973C00)
-
-
-
 val AmareloClaro2 = Color(0xFFFFF4C2)
-
 val IconBackgroundGreen = Color(0xFFD2F3B0)
 
 val IconBackgroundYellow = Color(0xFFFFF5D0)
-
 val IconBackgroundRed = Color(0xFFF8D7DA)
-
 val yellow2 = Color(0xFFFFC107)
-
 val incrementosgreen = Color(0xFF2ECC71)
-
 val BadgePositionColor = Color(0xFFAAAAAA)
-
 val BadgePositionColor2 = Color(0xFFCE8034)
-
 val BadgePositionColor3 = Color(0xFF566F7C)
-
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
+val Bluebutton = Color(0xFF006CFF)
 
 val DarkBlueBackground = Color(0xFF062452) // Fundo do BoxForm
 val PrimaryBlue = Color(0xFF0282EA)       // Botão "Entrar"
